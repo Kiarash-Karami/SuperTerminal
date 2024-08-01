aa0a93d3299ab735026cd2d783f62f0da95bd7f6
Kia Karami <Kiarash_Karami@outlook.com>
2024-08-01 19:31:24 +0330

    make it cleaner

--- main.py
+++ main.py
@@ -1,53 +1,15 @@
-from groq import Groq
 from os import system
-
-
-# replace 'your_api_key' with your Groq actual API key
-client = Groq(api_key='your_api_key')
-
-def Command_Generator(Prompt):
-
-    System_Prompt = "You are a Mac terminal expert who only provides the exact command for a given task. Do not provide any explanations or additional information, only the command. Make sure your response is in the format of text."
-
-    completion = client.chat.completions.create(
-        model="llama3-70b-8192",
-        messages=[
-            {
-                "role": "system",
-                "content": System_Prompt
-            },
-            {
-                "role": "user",
-                "content": Prompt
-            }
-        ],
-        temperature=1,
-        max_tokens=1024,
-        top_p=1,
-        stream=True,
-        stop=None,
-    )
-
-    Command_Text = ""
-    for chunk in completion:
-            chunk_content = chunk.choices[0].delta.content or ""
-            Command_Text += chunk_content
-    return Command_Text
-
-
-def System_Run(Command):
-    system(Command)
-
+from api_call import Command_Generator
 
 print('''Welcome to SuperTerminal! For quick navigation:
- - Enter 'q' to quit.
+ - Enter 'q' to quit.\n''')
 
-Enjoy your experience!\n''')
 User_prompt = input("$ ")
 
 while User_prompt.lower() != "q":
     final_command = Command_Generator(User_prompt)
     print(f"Command: {final_command}")
-    System_Run(final_command)
+
+    system(final_command)
 
     User_prompt = input("$ ")
