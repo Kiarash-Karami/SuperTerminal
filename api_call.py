404d9b77250dc0be6993e4d052ae42e73adb6c78
Kia Karami <Kiarash_Karami@outlook.com>
2024-08-02 12:55:28 +0330

    Handle Some Error

--- api_call.py
+++ api_call.py
@@ -1,4 +1,5 @@
-from groq import Groq
+from groq import Client, Groq
+from error_handler import Client_Error
 
 # replace 'your_api_key' with your Groq actual API key
 client = Groq(api_key='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')
@@ -25,9 +26,21 @@ def Command_Generator(Prompt):
         stream=True,
         stop=None,
     )
+    try:
+        Command_Text = ""
+        for chunk in completion:
+                chunk_content = chunk.choices[0].delta.content or ""
+                Command_Text += chunk_content
 
-    Command_Text = ""
-    for chunk in completion:
-            chunk_content = chunk.choices[0].delta.content or ""
-            Command_Text += chunk_content
-    return Command_Text
+        return Command_Text
+
+    except:
+        error = completion.response
+        if error == Client_Error.Error_400.value:
+            print("The server could not understand the request due to invalid syntax. Review the request format and ensure it is correct.")
+        elif error == Client_Error.Error_404.value:
+            print("The requested resource could not be found. Check the request URL and the existence of the resource.")
+        elif error == Client_Error.Error_422.value:
+            print("The request was well-formed but could not be followed due to semantic errors. Verify the data provided for correctness and completeness.")
+        elif error == Client_Error.Error_429.value:
+            print("Too many requests were sent in a given timeframe. Implement request throttling and respect rate limits.")
