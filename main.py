84d411ba2ab70a68a23ec506768b3d34b126e4b8
Kia Karami <Kiarash_Karami@outlook.com>
2024-08-04 08:26:59 +0330

    Make The Ui little Better

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from api_call import Command_Generator
 from system_run import System_Run
 
-print('''Welcome to SuperTerminal! For quick navigation:
- - Enter 'q' to quit.\n''')
+print('''Welcome to SuperTerminal!\n''')
 
 User_prompt = input("$ ")
 
+GRAY = "\033[38;5;8m"
+RESET = "\033[0m"
+
 while User_prompt.lower() != "q":
     Terminal_Command = Command_Generator(User_prompt)
+    print(GRAY + "> " + Terminal_Command + RESET)
     System_Run(Terminal_Command)
     User_prompt = input("$ ")
